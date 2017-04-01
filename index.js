4829d0173697e5b25b37e2d4f47de43a22633397
Dayton Wiseman <dayton.a.wiseman@gmail.com>
2017-04-01 03:20:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+var array = []
+
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
+
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
+}
